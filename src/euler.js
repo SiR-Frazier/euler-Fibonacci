ceb9f4906a4f7a61d3d2257ec6f7bfa9aac125b4
Bryce Frazier and Emily Watkins <bryce+emily@>
2018-03-13 12:05:37 -0700

    add content to multiples function.

--- src/euler.js
+++ src/euler.js
@@ -3,4 +3,12 @@ export function Euler(number) {
 }
 
 Euler.prototype.multiples = function() {
+  var array = [];
+  for (var i = 1; i <= this.number; i += 1) {
+    array.push(i);
+  }
+  console.log(array);
+  return array;
+  // if (this.number % 3 === 0 || this.number % 5 === 0 )
+  //   return array.push(this.number)
 };
