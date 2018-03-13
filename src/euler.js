86bf72f96b3a2c7be1657f92a8b5f070c028a297
Bryce Frazier and Emily Watkins <bryce@>
2018-03-13 13:41:49 -0700

    add logic to euler.js and 1 passing test

--- src/euler.js
+++ src/euler.js
@@ -7,8 +7,12 @@ Euler.prototype.multiples = function() {
   for (var i = 1; i <= this.number; i += 1) {
     array.push(i);
   }
-  console.log(array);
-  return array;
-  // if (this.number % 3 === 0 || this.number % 5 === 0 )
-  //   return array.push(this.number)
+
+  var new_array = [];
+  for (var j = 1; j < array.length; j++) {
+    if ((j % 3 === 0) || (j % 5 === 0)) {
+      new_array.push(j);
+    }
+  }
+  return new_array;
 };
